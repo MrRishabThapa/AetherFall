af737f343da65aeba3e70baced6708720b29a995
Rishab <rishabthapa7777@gmail.com>
2026-07-17 08:31:59 +0545

    pics

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ const processItems: ProcessItem[] = [
   },
 
   { type: "image", src: "/process/story.png", title: "Game UI concept" },
-
+{ type: "image", src: "/process/generation.jpeg", title: "Game UI concept" },
   {
     type: "image",
     src: "/process/character-custom-design.png",
